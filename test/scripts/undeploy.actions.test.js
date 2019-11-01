fa121a1975a8345566235340d7e352c2811d83b0
Moritz Raho <raho@adobe.com>
2019-11-01 14:37:53 +0100

    removed uneeded spawn io runtime func

--- test/scripts/undeploy.actions.test.js
+++ test/scripts/undeploy.actions.test.js
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-const { vol, fs } = global.mockFs()
+const { vol } = global.mockFs()
 
 const AppScripts = require('../..')
 
