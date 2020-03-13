fe4c4abf2a7b75d7ac63bf11d2256214c84dd951
Tania Mathern <mathern@adobe.com>
2020-03-13 11:32:54 -0700

    Added empty tests

---
+++ test/lib/wskprops.test.js
@@ -0,0 +1,45 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+'use strict'
+
+const wskprops = require('../../lib/wskprops')
+
+test('Gets wskprop file from env', async () => {
+    
+})
+
+test('Gets wskprop file from default location', async () => {
+    
+})
+
+// tests exported function hasWskConfig()
+test('Checks if there is a default wsk config or not', async () => {
+    
+})
+
+test('Reads wskprops config', async () => {
+    
+})
+
+test('Gets wsk props', async () => {
+    
+})
+
+test('Gets wsk env props', async () => {
+    
+})
+
+// tests exported function get()
+test('Aggregates information from wskprops', async () => {
+    
+})
